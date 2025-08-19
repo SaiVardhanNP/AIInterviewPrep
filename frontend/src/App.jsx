93b351bf049a96b475ccca5c18cc4c3264579def
SaiVardhanNP <saivardhan.nyalapatla55@gmail.com>
2025-08-19 22:34:15 +0530

    fixed the signup typo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router,Routes, Route  } from 'react-router-dom';
 import {Toaster} from 'react-hot-toast';
 import Login from './pages/Auth/Login';
-import Signup from './pages/Auth/SignUp';
+import SignUp from './pages/Auth/SignUp';
 import Dashboard from './pages/Home/Dashboard';
 import LandingPage from './pages/LandingPage';
 import InterviewPrep from './pages/InterviewPrep/InterviewPrep';
@@ -17,7 +17,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+          <Route path="/signup" element={<SignUp />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/interview-prep/:sessionId" element={<InterviewPrep />} />  
         </Routes>
